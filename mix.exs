4e2dd9889245f3cb56560060f6073c4279fd1697
Stefan Breunig <stefan-github@yrden.de>
2024-04-11 15:32:47 +0200

    dependency updates

--- mix.exs
+++ mix.exs
@@ -8,8 +8,8 @@ defmodule Tqdm.Mixfile do
       app: :tqdm,
       version: @version,
       elixir: "~> 1.4",
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
       package: package(),
@@ -20,18 +20,21 @@ defmodule Tqdm.Mixfile do
         source_ref: "v#{@version}"
       ],
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
   defp deps do
     [
-      {:credo, "~> 0.3", only: [:dev, :test]},
-      {:dialyxir, "~> 0.3", only: [:dev, :test]},
-      {:earmark, "~> 1.2", only: :dev},
-      {:ex_doc, "~> 0.18", only: :dev},
-      {:excoveralls, "~> 0.4", only: :test},
-      {:inch_ex, ">= 0.0.0", only: :docs}
+      {:credo, "~> 1.7.5", only: [:dev, :test]},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.32", only: :dev},
+      {:excoveralls, "~> 0.18", only: :test}
     ]
   end
 
